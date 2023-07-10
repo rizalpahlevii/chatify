8336e3ac9718770f7a909900a9ef18634a1f0e8b
Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2023-07-11 01:51:43 +0700

    Prepare encryption

--- app/Models/ChMessage.php
+++ app/Models/ChMessage.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Chatify\Traits\UUID;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class ChMessage extends Model
 {
     use UUID;
+
+    public function setBodyAttribute($value)
+    {
+        $this->attributes['body'] = encrypt($value);
+    }
+
+    public function getBodyAttribute()
+    {
+        return decrypt($this->attributes['body']);
+    }
 }
